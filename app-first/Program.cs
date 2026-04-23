2157e0d960f4c69033cbe6dca66d7572264bf55d
Carol <carolinevictoria1224@gmail.com>
2026-04-23 10:51:36 -0300

    Estrutura base da aplicacao e funcao de registrar banda

---
+++ app-first/Program.cs
@@ -0,0 +1,42 @@
+﻿void exibirMenu()
+{
+    Console.WriteLine("--------Boas Vindas--------");
+    Console.WriteLine("1. Para registrar uma banda");
+    Console.WriteLine("1. Para mostrar todas as bandas");
+    Console.WriteLine("1. Para avaliar uma banda");
+    Console.WriteLine("1. Para mostrar a média uma banda");
+    Console.WriteLine("5. Para sair");
+
+    Console.Write("Digite sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoNumerica = int.Parse(opcaoEscolhida);
+
+    switch(opcaoNumerica){
+        case 1: registrarBanda();
+            break;
+        case 2: Console.WriteLine("Voce escolheu a opcao " + opcaoNumerica);
+            break;
+        case 3: Console.WriteLine("Voce escolheu a opcao " + opcaoNumerica);
+            break;
+        case 4: Console.WriteLine("Voce escolheu a opcao " + opcaoNumerica);
+            break;
+        case 5: Console.WriteLine("Voce escolheu sair.");
+            break;
+        default: Console.WriteLine("Digite uma opção valida!");
+            break;
+    }
+}
+
+void registrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas.");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda {nomeBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    exibirMenu();
+}
+
+exibirMenu();
