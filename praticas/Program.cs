ba8441ddcf6bd497dd22e19801c3da2f22d0bbe6
Carol <carolinevictoria1224@gmail.com>
2026-04-24 13:54:37 -0300

    Milhas em quilometros

--- praticas/Program.cs
+++ praticas/Program.cs
@@ -14,3 +14,10 @@ char tipoConta = 'P';
 Console.WriteLine("Valor recebido: R$" + valorRecebido);
 Console.WriteLine("Doação anônima: " + doacaoAnonima);
 Console.WriteLine("Tipo de conta: " + tipoConta);
+
+// Milhas em quilometros
+double distancia = 10.0;
+double milhasConvert = distancia * 1.60934;
+
+Console.WriteLine($"{distancia} milhas são equivalentes a {milhasConvert} quilômetros!");
+
