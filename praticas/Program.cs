845f75734dd5218aa0fa1751e1952572b4667de9
Carol <carolinevictoria1224@gmail.com>
2026-04-24 13:47:58 -0300

    Registro de doações

--- praticas/Program.cs
+++ praticas/Program.cs
@@ -5,3 +5,12 @@ int anoConvertido = int.Parse(anoNascimento);
 
 int calculo = 2026 - anoConvertido;
 Console.WriteLine($"Sua idade é {calculo}");
+
+// Registro de doações
+float valorRecebido = 500.00f;
+bool doacaoAnonima = true;
+char tipoConta = 'P';
+
+Console.WriteLine("Valor recebido: R$" + valorRecebido);
+Console.WriteLine("Doação anônima: " + doacaoAnonima);
+Console.WriteLine("Tipo de conta: " + tipoConta);
