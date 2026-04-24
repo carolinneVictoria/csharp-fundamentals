3c4c860ab97362dc99c2952d664a964a1c0f6ccc
Carol <carolinevictoria1224@gmail.com>
2026-04-24 16:07:37 -0300

    Adicionando produtos no estoque

--- praticas/Program.cs
+++ praticas/Program.cs
@@ -44,3 +44,22 @@ do
     total += valor;
 } while (valor != 0);
 Console.WriteLine("Total de vendas do dia: R$" + total);
+
+// Produtos no estoque
+int estoque = 0;
+Console.WriteLine("Deseja adicionar um produto ao estoque?");
+Console.WriteLine("1 - Sim | 0 - Não");
+int resposta = int.Parse(Console.ReadLine()!);
+
+while (resposta == 1)
+{
+    Console.WriteLine("Quantidade:");
+    int quantidade = Convert.ToInt32(Console.ReadLine());
+    estoque += quantidade;
+
+    Console.WriteLine($"Estoque atual: {estoque}");
+
+    Console.WriteLine("Deseja continuar?");
+    Console.WriteLine("1 - Sim | 0 - Não");
+    resposta = int.Parse(Console.ReadLine()!);
+};
