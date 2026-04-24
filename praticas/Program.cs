1cb45311c5de18b313fb58bb936c17ec4793fb14
Carol <carolinevictoria1224@gmail.com>
2026-04-24 13:44:33 -0300

    Exercicio calculando a idade

---
+++ praticas/Program.cs
@@ -0,0 +1,7 @@
+﻿// Calculando a idade
+Console.Write("Digite seu ano de nascimento: ");
+string anoNascimento = Console.ReadLine()!;
+int anoConvertido = int.Parse(anoNascimento);
+
+int calculo = 2026 - anoConvertido;
+Console.WriteLine($"Sua idade é {calculo}");
