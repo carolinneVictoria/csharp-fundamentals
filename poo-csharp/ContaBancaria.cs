ca17c3bf765f629cd29db469fc818e7fcc4a6f12
Carol <carolinevictoria1224@gmail.com>
2026-04-29 11:56:05 -0300

    Classes e métodos

---
+++ poo-csharp/ContaBancaria.cs
@@ -0,0 +1,16 @@
+﻿namespace poo_csharp;
+
+public class ContaBancaria
+{
+    public int NumeroIndicador { get; set; }
+    public string Titular { get; set; }
+    public float Saldo { get; set; }
+    public string Senha { get; set; }
+
+    public void exibirDados()
+    {
+        Console.WriteLine($"Número do Indicador: {NumeroIndicador}");
+        Console.WriteLine($"Titular: {Titular}");
+        Console.WriteLine($"Saldo: {Saldo}");
+    }
+}
---
+++ poo-csharp/Program.cs
@@ -0,0 +1,8 @@
+﻿using poo_csharp;
+
+ContaBancaria contaBancaria= new ContaBancaria();
+contaBancaria.NumeroIndicador = 12345;
+contaBancaria.Titular = "João Silva";
+contaBancaria.Saldo = 1000.50f;
+contaBancaria.Senha = "senha123";
+contaBancaria.exibirDados();
